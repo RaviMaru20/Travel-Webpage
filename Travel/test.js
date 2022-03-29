f084f01d79abcd765ce223668d069b15bedb5cdc
RAVI MARU <33301239+RaviMaru20@users.noreply.github.com>
2022-03-29 10:37:30 -0400

    Uploaded local repo

---
+++ Travel/test.js
@@ -0,0 +1,16 @@
+var a = [0,1,0,0];
+
+function arr (array){
+    for(var i = 2; i < 5; i++){
+        array[i] = (i*(i-1))
+
+    }
+    console.log(array);
+
+}
+
+arr(a);
+
+var x = a[4]
+
+console.log(x)
